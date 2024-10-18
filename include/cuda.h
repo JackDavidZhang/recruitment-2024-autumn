001147602fcd1651e643e1ebbcd5beac656e3744
ztsubaki <2949125232@qq.com>
2024-10-18 16:53:11 +0800

    Init CUDA

---
+++ include/cuda.h
@@ -0,0 +1,8 @@
+//
+// Created by ztsubaki on 24-10-18.
+//
+
+#ifndef CUDA_H
+#define CUDA_H
+
+#endif //CUDA_H
---
+++ src/cuda.cu
@@ -0,0 +1,5 @@
+#include "cuda.h"
+
+__host__ void cuda_caluc(float *__restrict__ x, float *__restrict__ y) {
+
+}
