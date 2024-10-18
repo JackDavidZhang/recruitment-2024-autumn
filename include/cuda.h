62324c95903172e96fd3337ea025c2efd42db19f
ztsubaki <2949125232@qq.com>
2024-10-18 18:53:52 +0800

    CDUA Caluc 1

--- include/cuda.h
+++ include/cuda.h
@@ -5,7 +5,6 @@
 #ifndef CUDA_H
 #define CUDA_H
 
-void __host__ cuda_caluc1(int * dst, unsigned * src, int n,int num);
-void __global__ cuda_cal1(int * dst, unsigned * src, int n,int num);
+void cuda_caluc1(int * dst, unsigned * src, int n,int num);
 
 #endif //CUDA_H
--- src/cuda.cu
+++ src/cuda.cu
@@ -1,16 +1,21 @@
 #include "cuda.h"
 
+void __global__ cuda_cal1(int * dst, unsigned * src, int n,int num);
+
 void __host__ cuda_caluc1(int * dst, unsigned * src, int n,int num) {
-  int *d_dst,*d_src;
+  int *d_dst;
+  unsigned *d_src;
   cudaMalloc((void**)&d_dst,n*sizeof(int));
   cudaMalloc((void**)&d_src,n*sizeof(unsigned));
   cudaMemcpy(d_src,src,n*sizeof(unsigned),cudaMemcpyHostToDevice);
-  cuda_cal1<<<n/1024+1,1024>>>(d_dst,d_src,n,num);
+  cuda_cal1<<<(n/512)+1,512>>>(d_dst,d_src,n,num);
   cudaMemcpy(dst,d_dst,n*sizeof(int),cudaMemcpyDeviceToHost);
+  cudaFree(d_dst);
+  cudaFree(d_src);
 }
 
 void __global__ cuda_cal1(int * dst, unsigned * src, int n,int num) {
-   int index = blockIdx.x*blockDim.x+threadIdx.x;
+   unsigned int index = blockIdx.x*blockDim.x+threadIdx.x;
    if(index<n) {
      dst[index] = src[index]+num;
     }
